53684b2b8cdfa0c6085c15b9b3b66a8a073c1a69
Vinicius Santos <santosvinicius.dev@gmail.com>
2024-04-23 12:05:33 -0300

    add: project structure
    Estrutura inicial de pastas do projeto
    - public_html/index.php
    - public_html/scr/ .svg icon files
    - public_html/template/footer.php | template/header.php
    - src/style/style.css root color variables 
    - docs/readme.md file (empty)

---
+++ public_html/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./src/style.css">
+  <title>TEAssist</title>
+</head>
+
+<body>
+  <main>
+    <nav>
+      <img src="/" alt="Logo TEAssist">
+      <div>
+        <ul>
+          <li>Sobre</li>
+          <li>Aplicativo</li>
+          <li>Equipe</li>
+        </ul>
+      </div>
+      <button>
+        <img src="/" alt="github icon">
+        <a href="/">Apoie</a>
+      </button>
+    </nav>
+
+    <div class="banner">
+      <div class="banner-text">
+        <h1>
+          <span>Facilite a Rotina!</span><br>
+          Conheça o aplicativo que melhora a rotina da criança com TEA.
+        </h1>
+      </div>
+      <img src="/" alt="banner image">
+    </div>
+  </main>
+  <section class="about-tea">
+    <h2>Sobre o TEA</h2>
+    <div>
+      <img src="/" alt="puzzle image">
+      <p>
+        O <span>Transtorno do Espectro Autista (TEA)</span> é uma condição caracterizado pela alteração das funções do neurodesenvolvimento do indivíduo, interferindo na capacidade de comunicação, linguagem, interação social e comportamento. É heterogênea, possuindo níveis variados de gravidade e expressão de sintomas como por exemplo epilepsia, dificuldade de aprendizagem ou distúrbios de sono.
+      </p>
+    </div>
+  </section>
+  <section class="about-teassist">
+    <h2>O TEAssist</h2>
+    <div>
+      <div>
+        <p>O aplicativo tem como objetivo de proporcionar um ambiente que viabilize o estabelecimento de rotinas de tarefas para o dia a dia da criança com TEA através de um trabalho conjunto dos pais com a equipe de apoio escolar. Levando em consideração que indivíduos com TEA apresentam dificuldade no processamento de estímulos de mudança e acontecimentos imprevisíveis, uma das funcionalidades do aplicativo é criação de eventos que registram atividades atípicas à rotina e que podem ocasionar comportamentos aversivos. </p>
+        <button>
+          <img src="/" alt="github icon">
+          <a href="/">Apoie o projeto</a>
+        </button>
+      </div>
+      <img src="/" alt="app logo">
+    </div>
+  </section>
+</body>
+
+</html>
