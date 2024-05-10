6bc5e6d508c85f889bf6298da14fa0ecbb17f3ba
0RyanSouza0 <tlousouza678@gmail.com>
2024-05-10 17:57:15 -0300

    Adicionando parte que faltava

--- public_html/index.php
+++ public_html/index.php
@@ -28,7 +28,7 @@ include './template/header.php';
               Nosso aplicativo permite uma colaboração eficaz entre pais e equipe escolar, fornecendo ferramentas
               para estabelecer e manter rotinas de tarefas específicas para crianças com Transtorno do Espectro Autista (TEA)
             </p>
-            <button><a href="/">Ver Mais</a></button>
+            <a href="/">Ver Mais</a>
           </div>
             <img src="./src/images/puzzle.png" alt="banner image">
         </div>
@@ -50,20 +50,60 @@ include './template/header.php';
           <h2>O TEAssist</h2>
 
           <p> O aplicativo tem como objetivo de proporcionar um ambiente que viabilize o estabelecimento de rotinas de tarefas para o dia a dia da criança com TEA através de um trabalho conjunto dos pais com a equipe de apoio escolar. Levando em consideração que indivíduos com TEA apresentam dificuldade no processamento de estímulos de mudança e acontecimentos imprevisíveis, uma das funcionalidades do aplicativo é criação de eventos que registram atividades atípicas à rotina e que podem ocasionar comportamentos aversivos. </p>
-          <button>
-            <img src="./src/images/github_icon.svg" alt="github icon">
-            <a href="/">Apoie o projeto</a>
-          </button>
+
+            <a href="/"><i class="fa-brands fa-github"></i>  Documentação </a>
+
+
         </div>
         <img src="./src/images/logo_main.svg" alt="app logo">
     </section>
-  </div>
-  <footer>
-    <img src="" alt="">
-    <p>TEAssist</p>
-    <span>  </span>
-  </footer>
 
+    <section class="services">
+
+        <h2>Serviços/Objetivos</h2>
+      <div class="align-itens">
+        <div class="card-service">
+          <img src="./src/images/brain.svg" alt="foto do card de serviços">
+          <p>Mapeamento de</p>
+          <p>comportamentos</p>
+        </div>
+
+        <div class="card-service">
+          <img src="./src/images/calendar.svg" alt="foto do card de serviços">
+          <p>Registro de Rotina </p>
+          <p>de Eventos</p>
+        </div>
+
+        <div class="card-service">
+          <img src="./src/images/puzzle.svg" alt="foto do card de serviços">
+          <p>Tratamento</p>
+          <p>Mulltidisciplinar</p>
+        </div>
+
+        <div class="card-service">
+          <img src="./src/images/talk.svg" alt="foto do card de serviços">
+          <p>Comunicação</p>
+          <p>entre Cuidadores</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="project">
+      <h2>Acompanhe o Projeto</h2>
+      <div class="align-itens">
+        <img src="./src/images/puzzle.png" alt="Foto do Projeto">
+        <div class="text-project">
+          <p>O <span>TEAssist</span> está sendo desenvolvido através da disciplina de Projeto Integrador  do CST <span>Análise e Desenvolvimento de Sistemas na  Fatec Ferraz de Vasconcelos.</span> Você pode acompanhar as etapas do projeto através do GitHub, onde está disponivel todo o código deste site, das próximas etapas
+          do aplicativo e também a documentação técnica do projeto.</p>
+          <p >Que tal deixar uma estrela no repositório e acompanhar nossa jornada?</p>
+
+          <a href="#"><i class="fa-solid fa-star"></i> Apoie o Projeto</a>
+        </div>
+      </div>
+    </section>
+  </div>
+  <?php include './template/footer.php';?>
+  <script src="https://kit.fontawesome.com/1c065add65.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- public_html/template/footer.php
+++ public_html/template/footer.php
@@ -5,14 +5,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./src/footer.css">
+  <link rel="stylesheet" href="./src/style/footer.css">
   <title>TEAssist</title>
 </head>
 
 <body>
-    <footer>
-        
-    </footer>
+  <footer>
+    
+    <p>FATEC - Ferraz de Vasconcelos </p>
+    <span></span>
+    <p>Projeto Integrador  </p>
+    <span></span>
+    <p>2024 @ </p>
+    
+  </footer>
 </body>
 
 </html>
